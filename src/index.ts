7b94b77cb9dd08bacbf09c4f17bd0ee3a69dc8f3
unknown <fahmi.mellow@gmail.com>
2024-01-30 17:00:23 +0700

    adjustment

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,27 @@
-import { AppDataSource } from "./data-source"
-import * as express from "express"
-import routes from "./route"
-import "dotenv/config"
-import * as cors from "cors"
-import cookieParser = require("cookie-parser")
-import session = require("express-session")
+import { AppDataSource } from "./data-source";
+import * as express from "express";
+import routes from "./route";
+import "dotenv/config";
+import * as cors from "cors";
+import cookieParser = require("cookie-parser");
 
-// type User = {
-//     id: number
-//     username: string
-//     isAdmin: boolean
-// }
+AppDataSource.initialize()
+    .then(async () => {
+        const app = express();
 
-// declare module "express-session" {
-//     interface SessionData {
-//         user: User
-//     }
-// }
+        app.use(
+            cors({
+                credentials: true,
+                origin: "http://localhost:5173",
+                methods: ["GET", "POST", "PATCH", "DELETE"],
+                allowedHeaders: ["Origin", "Content-Type", "Authorization", "Accept"],
+                preflightContinue: true,
+            })
+        );
+        app.use(cookieParser());
+        app.use(express.json());
+        app.use("/api/v1", routes);
 
-AppDataSource.initialize().then(async () => {
-    const app = express()
-
-    // app.use(session({
-    //     secret: process.env.SECRET_KEY,
-    //     resave: false,
-    //     saveUninitialized: true,
-    //     cookie: {
-    //         secure: "auto"
-    //     }
-    // }))
-    app.use(cors({
-        credentials: true,
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST", "PATCH", "DELETE"],
-        allowedHeaders: ["Origin", "Content-Type", "Authorization", "Accept"],
-        preflightContinue: true
-    }))
-    app.use(cookieParser())
-    app.use(express.json())
-    app.use("/api/v1", routes)
-
-    app.listen(process.env.PORT, () => console.log(`Server is running!`))
-
-}).catch(error => console.log(error))
+        app.listen(process.env.PORT, () => console.log(`Server is running!`));
+    })
+    .catch((error) => console.log(error));
